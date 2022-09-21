fab3053fac7984157fadb5d7114c557c18886d5b
Maureen <chiamaureenla@gmail.com>
2022-09-21 19:40:36 +0800

    main page - hero section done

---
+++ src/components/Main.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="main">
+        <ToolBar/>
+        <div class="hero-section">
+            <div class="heto-title">
+                <div class="title">PLANTA</div>
+                <div class="subtitle">plant shop</div>
+            </div>
+            <div class="hero-img"></div>
+        </div>
+    </div>
+</template>
+<script>
+import ToolBar from "./ToolBar.vue";
+export default {
+    components: {
+        ToolBar
+    },
+
+    data () {
+        return {
+            
+        }
+    },
+
+    methods: {
+    }
+}
+
+</script>
+
+<style scoped>
+    .main {
+        position: relative;
+    }
+
+    .hero-section {
+        padding: 3rem 6rem 0 12rem;
+    }
+
+    .heto-title {
+        position: absolute;
+        top: 28%;
+        left: 10%;
+    }
+
+    .heto-title .title {
+        font-size: 70px;
+        font-weight: 600;
+        letter-spacing: 5px;
+    }
+
+    .heto-title .subtitle {
+        font-size: 20px;
+        font-weight: 500;
+        letter-spacing: 5px;
+        text-align: center;
+    }
+
+    .hero-img {
+        max-height: 1200px;
+        height: 95vh;
+        width: 100%;
+        background-image: url("../assets/img/hero_img_pc.jpeg");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+</style>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="toolbar">
+        
+    </div>
+</template>
+<script>
+
+export default {
+    components: {
+    },
+
+    data () {
+        return {
+            
+        }
+    },
+
+    methods: {
+    }
+}
+
+</script>
+
+<style scoped>
+    .toolbar {
+        height: 100vh;
+        width: 80px;
+        background-color: #eee;
+        position: absolute;
+        top: 0
+    }
+</style>
